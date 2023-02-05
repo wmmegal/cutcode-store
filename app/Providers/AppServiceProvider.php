e549fca488530b1491ed86a0cea705e58aafe0f1
wmmeg <wmmegal@gmail.com>
2023-02-05 14:13:14 +0200

    init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Kernel;
+use Carbon\CarbonInterval;
+use DB;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Model::preventLazyLoading(!app()->isProduction());
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+
+        DB::whenQueryingForLongerThan(500, function (Connection $connection) {
+            logger()
+                ->channel('telegram')
+                ->debug('whenQueryingForLongerThan:' . $connection->query()->toSql());
+        });
+
+        $kernel = app(Kernel::class);
+        $kernel->whenRequestLifecycleIsLongerThan(
+            CarbonInterval::seconds(4),
+            function () {
+                logger()
+                    ->channel('telegram')
+                    ->debug('whenRequestLifecycleIsLongerThan:' . request()->url());
+            }
+        );
+    }
+}
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/sass/main.sass',
+                'resources/js/app.js'
+            ],
+            refresh: true,
+        }),
+    ],
+});
